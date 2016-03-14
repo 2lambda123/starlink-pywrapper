c16451e4a5ae6a5850378bd431060836fd92b453
Sarah F Graves <s.graves@eaobservatory.org>
2016-03-14 10:03:37 -1000

    init: import modules normally

--- star/__init__.py
+++ star/__init__.py
@@ -1,7 +1,7 @@
 from star import *
-from smurf import *
-from cupid import *
-from kappa import *
-from convert import *
-from figaro import *
-from surf import *
+import smurf
+import cupid
+import kappa
+import convert
+import figaro
+import surf
